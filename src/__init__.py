fd3cd1557634d8a3a917f1c75c77477a67f6f445
Dennis Dang <dennisvdang@gmail.com>
2025-04-03 15:39:35 -0700

    Create __init__.py

---
+++ src/__init__.py
@@ -0,0 +1,14 @@
+"""
+Indeed Job Scraper
+
+A package for scraping job listings from Indeed.com with support for filtering and
+extracting detailed job descriptions.
+
+This package consists of two main modules:
+- indeed_scraper: Main module for searching and extracting job listings
+- description_scraper: Specialized module for job description extraction
+
+Author: Dennis
+"""
+
+__version__ = "0.1.0" 
